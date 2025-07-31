8879572b9fd698d6a97be0759f99185c8bf46576
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-07-31 09:49:59 +0600

    Update settings.py
    j

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from datetime import timedelta 
 from pathlib import Path 
-import os
+import os 
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
