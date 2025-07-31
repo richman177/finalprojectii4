2dd03d136c9735cfc9384884a4aa69598cc223ab
Elmirbek Toktoraliev <elmirbektoktoraliev06@gmail.com>
2025-07-31 09:49:45 +0600

    Update settings.py
    r

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from datetime import timedelta 
-from pathlib import Path
+from pathlib import Path 
 import os
 from dotenv import load_dotenv
 
